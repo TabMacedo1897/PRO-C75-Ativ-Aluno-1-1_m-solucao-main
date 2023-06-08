571b3d79f342bbb531abe164cd43ae4916667a39
m1gu3l022 <tabata.macedo1897@gmail.com>
2023-06-08 17:47:07 -0300

    Aula C75 Biblioteca Eletrônica

---
+++ PRO-C75-Ativ-Aluno-1-1_m-solucao-main/components/BottomTabNavigator.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+//importe as telas de navegação
+
+//crie a constante de navegação
+
+export default class BottomTabNavigator extends Component{
+    render(){
+        return(
+            // use as tags necessárias para criar a navegação
+        )
+    }
+}
---
+++ PRO-C75-Ativ-Aluno-1-1_m-solucao-main/screens/Transaction.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+//crie a tela de transação
